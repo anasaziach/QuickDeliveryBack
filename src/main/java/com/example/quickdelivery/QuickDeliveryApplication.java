e4f921ddd947ba241ab52d5fc75967ab1b6e52d7
Anasa <hamidlmath534@gmail.com>
2023-04-27 20:29:42 +0100

    first Push

---
+++ src/main/java/com/example/quickdelivery/QuickDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quickdelivery/QuickDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
